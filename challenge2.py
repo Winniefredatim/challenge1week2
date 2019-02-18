b54712e345d2e235e0bb7aef1731d9538364bd63
Winniefredatim <winniefredatimplexa@gmail.com>
2019-02-18 06:13:56 +0300

    make changes

--- challenge2.py
+++ challenge2.py
@@ -1,18 +1,24 @@
 
 from datetime import datetime
-def ageBracket():
+#in order to get the current year, import datetime.
+# create a function that holds the users age. 
+def my_age():
     try:
         current_year=datetime.now().year
-        inputyear=int(input("Please enter your year of Birth \n"))
+#create a variable that allows a user to enter his/her year of birth.
+        inputyear=int(input("enter your year of Birth \n"))
+#get current ade of the user.
         current_age = current_year - inputyear
         if(current_age <18):
                 print("You are a minor")
-                #elif(person_age>=18):
-                #print("You are a youth")
+        elif(current_age>=18 and current_age<36):
+                print("You are a youth")
         else:
              print("You are an elder")
     except ValueError:
-        print("This is not a valid year")
+        print("not a valid year")
+    
+             
 
 if __name__=="__main__":
-    ageBracket()
+    my_age()
---
+++ myvowels.py
@@ -0,0 +1,24 @@
+def my_Vowels(stringInput):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    listFoundVowels = []
+    StringChars = set()
+    DuplicatedChars = set()
+
+    try:
+        stringInput = stringInput.replace(' ', '')
+
+        for each in stringInput:
+            if (each in vowels and each not in listFoundVowels):
+                listFoundVowels.append(each)
+            if(each in StringChars):
+                DuplicatedChars.add(each)
+            else:
+                StringChars.add(each)
+
+            stringvoweltuple = ''.join(tuple(listFoundVowels))
+        return (stringvoweltuple, len(DuplicatedChars))
+    except TypeError:
+        return("Invalid input")
+
+print(my_Vowels('dahdah'))
+print(my_Vowels('drink water')) 
